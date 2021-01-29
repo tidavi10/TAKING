72527cdee28f78ac8a3d4f89dea5e5cac37b9f83
tidavi10 <tidavi10@gmail.com>
2021-01-29 11:05:08 -0300

    Projeto atualizado 29/01 Python API

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import requests
 
 def main():
     print('####################')
+    print('DATA DE HOJE 29/01')
     print('##EXERCÍCIO BRANCH##')
     print('## TAKING CEP TESTE DAVI ##')
+    print('## TESTE DAVI ##')
+    print('## 123456 7890 ##')
+    print('ESTUDANDO PARA EFETUAR MERGE')
+    print('####################')
+    print('####################')
     print('####################')
     print()
 
